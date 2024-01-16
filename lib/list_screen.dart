d1af31d86075bd046bd11a98462b3e9f676b1a48
mybuenoday <yoonjibiz@gmail.com>
2024-01-16 11:55:18 +0900

    feat: update ListScreen to display todos retrieved from Hive database

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todolist/create_screen.dart';
-import 'package:todolist/todo.dart';
+import 'package:todolist/main.dart';
 
 class ListScreen extends StatefulWidget {
   const ListScreen({Key? key}) : super(key: key);
@@ -10,17 +10,6 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
-  final todos = [
-    Todo(
-      title: 'title 1',
-      dateTime: 12321,
-    ),
-    Todo(
-      title: 'title 1',
-      dateTime: 12321,
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,19 +17,18 @@ class _ListScreenState extends State<ListScreen> {
         title: const Text('To Do List'),
       ),
       body: ListView(
-        children: todos
+        children: todos.values
             .map((todo) => ListTile(
-          title: Text(todo.title),
-          subtitle: Text('${todo.dateTime}'),
-        ))
+                  title: Text(todo.title),
+                  subtitle: Text('${todo.dateTime}'),
+                ))
             .toList(),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           Navigator.push(
-              context,
-              MaterialPageRoute(builder:
-          (context) => const CreateScreen()),
+            context,
+            MaterialPageRoute(builder: (context) => const CreateScreen()),
           );
         },
         child: const Icon(Icons.add),
