f4a9300e00e6e201959f0d4666e1980b2d658782
mybuenoday <yoonjibiz@gmail.com>
2024-01-16 13:45:58 +0900

    feat: add new todo creation to CreateScreen
    - Implement todo addition with TextEditingController in CreateScreen.
    - Enable saving new todos to Hive database and navigate back on submission.
    - 새로운 todo 입력, done 버튼을 누르면, db에 저장, 저장 끝나고 뒤로 가기

--- lib/create_screen.dart
+++ lib/create_screen.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/main.dart';
+import 'package:todolist/todo.dart';
 
-class CreateScreen extends StatelessWidget {
+class CreateScreen extends StatefulWidget {
   const CreateScreen({Key? key}) : super(key: key);
 
+  @override
+  State<CreateScreen> createState() => _CreateScreenState();
+}
+
+class _CreateScreenState extends State<CreateScreen> {
+  final _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    // 화면을 벗어날 때 메모리 해제
+    _textController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Todo List'), actions: [
         IconButton(
-          onPressed: () {},
+          onPressed: () async {
+            await todos.add(Todo(
+              title: _textController.text,
+              dateTime: DateTime.now().millisecondsSinceEpoch, // int값으로 변환
+            ));
+            if (mounted) {
+              Navigator.pop(context);
+            }
+          },
           icon: const Icon(Icons.done),
         )
       ]),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: TextField(
+          controller: _textController,
             decoration: InputDecoration(
           border: const OutlineInputBorder(
             // borderRadius: BorderRadius.circular(10.0), ; make private가 떠서 아래 코드 찾아봄.
